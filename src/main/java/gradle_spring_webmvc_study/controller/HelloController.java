a30eb517aba8d40d6b7dd3365aacc9ed67078749
yoon <lrzrlove@naver.com>
2020-10-20 09:41:31 +0900

    설명추가

--- src/main/java/gradle_spring_webmvc_study/controller/HelloController.java
+++ src/main/java/gradle_spring_webmvc_study/controller/HelloController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
+	
+	//	/hello경로로 들어온 요청을 hello() 메소드를 이용하여 처리
+	//	Model Parameter는 컨트롤러의 처리 결과를 뷰에 전달할 때 사용
+	//	@RequestParam : Http 요청 parameter value 메소드의 parameter로 전달
+	//	@RequestParam("가져올 데이터의 이름") [데이터타입] [가져온데이터를 담을 변수]
 
 	@GetMapping("/hello")
 	public String hello(Model model, @RequestParam(value = "name", required = false) String name) {
